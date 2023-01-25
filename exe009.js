5a63cd12b92fd3dd3e68b097d9f1c01270877215
diogopolesso <diogopolesso@gmail.com>
2023-01-25 15:55:42 -0300

    09- Condição Simples

---
+++ exe009.js
@@ -0,0 +1,7 @@
+let vel = 60.5;
+console.log(`A Velocidade do seu carro é ${vel}`);
+
+if (vel > 60){ //condição Simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
+console.log(`Dirija sempre usando cinto de segurança`);
