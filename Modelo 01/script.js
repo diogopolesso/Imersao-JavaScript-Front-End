0d5237ab7debc0404112c15d65e2855ab8c15da6
diogopolesso <diogopolesso@gmail.com>
2023-01-26 17:37:41 -0300

    Modelo Web 1

--- Modelo 01/script.js
+++ Modelo 01/script.js
@@ -1,1 +1,21 @@
-alert('Ola Mundo')
+function carregar() {
+    let msg = document.getElementById('msg');
+    let img = document.getElementById('imagem');
+    let data = new Date();
+    let hora = data.getHours();
+        msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // "BOM DIA!"
+        img.src = 'img/dia.png'
+        document.body.style.background = '#f4d544'
+    }else if (hora >= 12 && hora <= 18) {
+        //"BOA TARDE!"
+        img.src = 'img/tarde.png'
+        document.body.style.background = '#937D6F'
+    }else {
+        //"BOA NOITE!"
+        img.src = 'img/noite.png'
+        document.body.style.background = '#4e6688'
+    }
+}
+
