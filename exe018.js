dbeb69d54b754b3115024dca81c1e2597208d719
diogopolesso <diogopolesso@gmail.com>
2023-02-01 21:12:22 -0300

    12|2- Instrução for

---
+++ exe018.js
@@ -0,0 +1,7 @@
+// for cria um loop que consiste em três expressões (inicialização, condição, iteração)
+
+console.log('Vai Iniciar!')
+for (let c = 1; c <= 10; c++) {
+    console.log(`Conta ${c}`)
+}
+console.log('Terminou!')
