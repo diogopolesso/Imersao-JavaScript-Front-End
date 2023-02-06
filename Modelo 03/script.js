41808cbd76e764b973386f4e3e1d722408157d29
diogopolesso <diogopolesso@gmail.com>
2023-02-06 15:17:11 -0300

    Modelo Web 3

--- Modelo 03/script.js
+++ Modelo 03/script.js
@@ -1,1 +1,11 @@
-const
+function contar() {
+    let inicio = documentqueryselector('div#txti');
+    let fim = documentqueryselector('div#txtf');
+    let contar = documentqueryselector('div#txtp');
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || PaymentResponse.value.length == 0) {
+        window.alert('[ERRO] Faltam Dados!')
+    } else {
+        alert('Tudo ok!')
+    }
+}
