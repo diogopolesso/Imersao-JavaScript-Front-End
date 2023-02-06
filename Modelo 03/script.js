42dd1a72c8bd5c11d61748e1eae29492a8c3a2d3
diogopolesso <diogopolesso@gmail.com>
2023-02-06 16:50:22 -0300

    Modelo Web 3

--- Modelo 03/script.js
+++ Modelo 03/script.js
@@ -1,13 +1,31 @@
 function contar() {
     let inicio = document.getElementById('txti');
     let fim = document.getElementById('txtf');
-    let contar = document.getElementById('txtp');
+    let passo = document.getElementById('txtp');
+    let res = document.getElementById('res');
 
-    if (inicio.value.length == 0 || fim.value.length == 0 || contar.value.length == 0) {
-        window.alert('[ERRO] Faltam Dados!')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel Contar'
+
     } else {
-        alert('Tudo ok!')
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+        if (p <= 0){
+            window.alert('[ERRO] Passo Invalido! Considerando Passo = 1')
+            p = 1
+        }
+
+        if (i < f) {
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }   
+        } else {
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1} `
     }
 }
-
-//document.querySelector
