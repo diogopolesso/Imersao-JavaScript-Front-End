a75eb0115980b7ef72fc76932eca3f6a12588411
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-02-14 18:19:45 -0300

    Create exe019.js

---
+++ exe019.js
@@ -0,0 +1,10 @@
+let num = [5, 8, 2, 9, 3]
+
+num[3] = 6  // adiciona um vetor porem substitui o existente na possição 3
+num.push(7) // adiciona um Vetor no final do array
+num.length  // atributo - serve para saber o comprimento de um array
+num.sort()    // pega todos os valores e coloca em ordem crescente (cuidar a ordem caso tenha especificado um num.push)
+
+console.log(num)
+console.log(`Nosso Vetor tem ${num.length} posições`)
+console.log(`O Primeiro valor do vetor é ${num[0]}`)
