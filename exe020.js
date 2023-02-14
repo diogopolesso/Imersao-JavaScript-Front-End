2054d3dbd16b6b6ae33a8e426c05cf3e62ede2da
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-02-14 18:31:00 -0300

    13|1- Variáveis Compostas

---
+++ exe020.js
@@ -0,0 +1,8 @@
+// Maneira Tradicional de Percurso de exibição do Vetor
+
+let valores = [8, 1, 7, 4, 2, 9]
+    console.log(valores)
+
+for (let pos = 0; pos < valores.length; pos++) {
+    console.log(`A Posição ${pos} tem o valor ${valores[pos]}`)
+}
